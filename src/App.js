a8bb0dd51130b3e20a059cef925a89f55f8b716a
lovethy <lovethy980@gmail.com>
2020-03-12 21:13:05 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [{
+    'id' : '1',
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : 'Ronaldo',
+    'birthday' : '19820501',
+    'gender' : 'M',
+    'rank' : 'ST'
+  },
+  {
+    'id' : '2',
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : 'Messi',
+    'birthday' : '19850122',
+    'gender' : 'M',
+    'rank' : 'CF'
+  },
+  {
+    'id' : '3',
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : 'Mbape',
+    'birthday' : '20000122',
+    'gender' : 'M',
+    'rank' : 'RW'
+  }]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop Customer management System</h2>
+    <div>
+      { customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} rank={c.rank} /> ) })
+      }
     </div>
   );
 }
---
+++ src/components/Customer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Customer extends React.Component{
+    render() {
+        return (
+            <div>
+                <CustomerProfile id={this.props.id} name={this.props.name} image={this.props.image} />
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} rank={this.props.rank} />
+            </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component{
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="Profile Image" />
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.rank}</p>
+            </div>
+        )
+    }
+    
+}
+export default Customer;
