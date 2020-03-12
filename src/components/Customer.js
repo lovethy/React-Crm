2ae683ed129499009684154e25fd39a13a70fd59
lovethy <lovethy980@gmail.com>
2020-03-12 22:16:59 +0900

    Apply Materia-ui

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,37 +1,20 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     render() {
         return (
-            <div>
-                <CustomerProfile id={this.props.id} name={this.props.name} image={this.props.image} />
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} rank={this.props.rank} />
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.rank}</TableCell>
+            </TableRow>
         )
     }
 }
 
-class CustomerProfile extends React.Component{
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="Profile Image" />
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render(){
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.rank}</p>
-            </div>
-        )
-    }
-    
-}
 export default Customer;
